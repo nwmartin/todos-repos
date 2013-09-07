f405dcb10615799cb775e9d81cf4efc61fb9fefb
Nick Martin <martin.nick@gmail.com>
2013-09-07 14:35:43 -0400

    Listing repos.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var _ = require('underscore');
 var account = require('./account');
+var Org = require('./org').org;
+var Repo = require('./repo').repo;
 
 var port = 3001;
 
@@ -8,13 +11,43 @@ app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
+var GitHubApi = require("github");
+var github = new GitHubApi({
+    version: "3.0.0",
+    timeout: 5000
+});
+
+github.authenticate({
+    type: "basic",
+    username: account.username,
+    password: account.password
+});
+
 app.get('/', function(req, res){
-  res.render('repos', {
-    repos: [
-      {title:'herp'},
-      {title:'derp'}
-    ]
+
+  github.user.getOrgs({
+    user: account.username
+  }, function(error, orgs) {
+    orgs.forEach( function(orgJson) {
+      var org = new Org(orgJson);
+      if (org.title === 'RiparianData') {
+        github.repos.getFromOrg({
+          org: org.title
+        }, function(err, repos) {
+          repos.forEach(function(repoJson) {
+            org.addRepo(new Repo(repoJson));
+          });
+
+          res.render('repos', {
+            org: org
+          });
+
+        });
+      }
+    });
   });
+
+
 });
 
 app.listen(port);
